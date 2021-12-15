cf554684bb5c6947fd2dae9a5b1bdbd42eedc171
dikpython <dikshabhasarkar@gmail>
2021-12-15 18:41:41 +0530

    all commit

---
+++ firstapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def hello_world_view(request):
+    return HttpResponse('<h1>Hello This is Response From Django Application</h1>')
+
